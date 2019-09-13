7305480adfc5cf868515088e6fbc6b7f09491187
Amaka Anikwe <amakaanikwe@gmail.com>
2019-09-12 23:32:48 -0400

    React update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-this.state = {
-  singleDrip: "Choice of house blend or rotating single-origin drip coffee; decaf available on request $5",
-  pourOver: "Pour-over of rotating single-origin coffee $5.5",
-  cafeAuLait: "hoice of drip coffee with steamed milk on top $5.5",
-  blackTea: "Assortment of Black Teas $3",
-  greenTea: "Assortment of Green Teas $3",
-  breadBasket: "ssortment of fresh baked fruit breads and muffins $5.5",
-  granola: "Natural cereal of honey toasted oats, raisins, almonds and dates $8",
-  belgianWaffle: "Vanilla flavored batter with malted flour $10.5",
-}
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      singleDrip: "Choice of house blend or rotating single-origin drip coffee; decaf available on request $5",
+      pourOver: "Pour-over of rotating single-origin coffee $5.5",
+      cafeAuLait: "hoice of drip coffee with steamed milk on top $5.5",
+      blackTea: "Assortment of Black Teas $3",
+      greenTea: "Assortment of Green Teas $3",
+      breadBasket: "ssortment of fresh baked fruit breads and muffins $5.5",
+      granola: "Natural cereal of honey toasted oats, raisins, almonds and dates $8",
+      belgianWaffle: "Vanilla flavored batter with malted flour $10.5",
+    };
+  }
+
+};
 
 export default App;
