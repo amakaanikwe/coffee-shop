3ac8a8b5f2e4c56ee105be427139aff3a129ee52
Amaka Anikwe <amakaanikwe@gmail.com>
2019-09-12 23:16:42 -0400

    Updated app.js to include state object

--- src/App.js
+++ src/App.js
@@ -2,25 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+this.state = {
+  singleDrip: "Choice of house blend or rotating single-origin drip coffee; decaf available on request $5",
+  pourOver: "Pour-over of rotating single-origin coffee $5.5",
+  cafeAuLait: "hoice of drip coffee with steamed milk on top $5.5",
+  blackTea: "Assortment of Black Teas $3",
+  greenTea: "Assortment of Green Teas $3",
+  breadBasket: "ssortment of fresh baked fruit breads and muffins $5.5",
+  granola: "Natural cereal of honey toasted oats, raisins, almonds and dates $8",
+  belgianWaffle: "Vanilla flavored batter with malted flour $10.5",
 }
 
 export default App;
