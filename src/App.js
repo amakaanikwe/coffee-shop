198e7379b77cf9fd05c490c367cbdd22fab2f6d9
Amaka Anikwe <amakaanikwe@gmail.com>
2019-09-13 07:23:04 -0400

    Updated state object

--- src/App.js
+++ src/App.js
@@ -7,14 +7,41 @@ class App extends React.Component{
     super(props);
 
     this.state = {
-      singleDrip: "Choice of house blend or rotating single-origin drip coffee; decaf available on request $5",
-      pourOver: "Pour-over of rotating single-origin coffee $5.5",
-      cafeAuLait: "hoice of drip coffee with steamed milk on top $5.5",
-      blackTea: "Assortment of Black Teas $3",
-      greenTea: "Assortment of Green Teas $3",
-      breadBasket: "ssortment of fresh baked fruit breads and muffins $5.5",
-      granola: "Natural cereal of honey toasted oats, raisins, almonds and dates $8",
-      belgianWaffle: "Vanilla flavored batter with malted flour $10.5",
+      menu : [
+        {
+          id: 'Single Drip',
+          summmary: "Choice of house blend or rotating single-origin drip coffee; decaf available on request $5"
+        },
+        {
+          id:'Pour-Over',
+          summary: "Pour-over of rotating single-origin coffee $5.5"
+        },
+        {
+          id:'Cafe Au Lait',
+          summary: "hoice of drip coffee with steamed milk on top $5.5"
+        },
+        {
+          id:'Black Tea',
+          summary: "Assortment of Black Teas $3"
+        },
+        {
+          id:'Green Tea',
+          summary: "Assortment of Green Teas $3"
+        },
+        {
+          id:'Bread Basket',
+          summary: "ssortment of fresh baked fruit breads and muffins $5.5"
+        },
+        {
+          id:'Granola',
+          summary: "Natural cereal of honey toasted oats, raisins, almonds and dates $8"
+        },
+        {
+          id:'Belgian Waffle',
+          summary: "Vanilla flavored batter with malted flour $10.5"
+        }  
+        
+      ]
     };
   }
 
