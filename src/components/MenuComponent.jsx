515cf435eec08fc09e264b2d35e686e8957f89ca
Amaka Anikwe <amakaanikwe@gmail.com>
2019-09-14 17:11:32 -0400

    Updated component file names

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -7,11 +7,11 @@
                  <h1>Our Menu</h1><br></br>
                     <h4>Coffee & Tea</h4><br></br>
 
-                    <h4>{this.props.id[0]}</h4>
-                    <p>{this.props.summary[0]}</p><br></br>
+                    <h4>{this.props.menu}</h4>
+                    <p>{this.props.menu[0].summary}</p><br></br>
 
-                    <h4>POUR-OVER</h4>
-                    <p>Pour-over of rotating single-origin coffee $5.5</p><br></br>
+                    <h4>{this.props.id[1]}</h4>
+                    <p>{this.props.summary[1]}</p><br></br>
 
                     <h4>CCAFE AU LAIT</h4>
                     <p>Choice of drip coffee with steamed milk on top $5.5</p><br></br>
