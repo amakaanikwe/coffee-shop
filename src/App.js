712e41c0f99140db7a095f007b2afc45e7d8b8b0
Amaka Anikwe <amakaanikwe@gmail.com>
2019-09-25 23:07:57 -0400

    Created external inventory list

--- src/App.js
+++ src/App.js
@@ -8,50 +8,7 @@ class App extends React.Component{
     super(props);
 
     this.state = {
-      coffee : [
-        {
-          id:'Single Drip',
-          summmary: "Choice of house blend or rotating single-origin drip coffee; decaf available on request $5"
-        },
-        {
-          id:'Pour-Over',
-          summary: "Pour-over of rotating single-origin coffee $5.5"
-        },
-        {
-          id:'Cafe Au Lait',
-          summary: "hoice of drip coffee with steamed milk on top $5.5"
-        },
-        {
-          id:'Black Tea',
-          summary: "Assortment of Black Teas $3"
-        },
-        {
-          id:'Green Tea',
-          summary: "Assortment of Green Teas $3"
-        },   
-      ],
-      treats : [
-        {
-          id:'Bread Basket',
-          summary: "ssortment of fresh baked fruit breads and muffins $5.5"
-        },
-        {
-          id:'Granola',
-          summary: "Natural cereal of honey toasted oats, raisins, almonds and dates $8"
-        },
-        {
-          id:'Belgian Waffle',
-          summary: "Vanilla flavored batter with malted flour $10.5"
-        },
-        {
-          id:'Scrambled eggs',
-          summary: "Scrambled eggs, roasted red pepper and garlic, with green onions $7.5"
-        },
-        {
-          id:'Blueberry Pancakes',
-          summary: "With syrup, butter and lots of berries $8.5"
-        }, 
-      ]
+     
 
     };  
   }
