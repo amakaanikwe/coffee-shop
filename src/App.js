c812af631880fb90b991fc2cb0631c7763abe2e7
Amaka Anikwe <amakaanikwe@gmail.com>
2019-10-09 07:28:37 -0400

    Updated react comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import './inventory/Csinventory.js';
 import CoffeeTeaComp from './components/CoffeeTeaComp.jsx';
 import TreatsComp from './components/TreatsComp.jsx';
 
@@ -8,12 +7,51 @@ class App extends React.Component{
   constructor(props) {
     super(props);
 
-    const coffee = window.csinventory.coffee;
-    const treats = window.csinventory.treats
-
     this.state = {
-      coffee,
-      treats
+      coffee : [
+        {
+          id:'Single Drip',
+          summmary: "Choice of house blend or rotating single-origin drip coffee; decaf available on request $5"
+        },
+        {
+          id:'Pour-Over',
+          summary: "Pour-over of rotating single-origin coffee $5.5"
+        },
+        {
+          id:'Cafe Au Lait',
+          summary: "hoice of drip coffee with steamed milk on top $5.5"
+        },
+        {
+          id:'Black Tea',
+          summary: "Assortment of Black Teas $3"
+        },
+        {
+          id:'Green Tea',
+          summary: "Assortment of Green Teas $3"
+        }   
+      ],
+      treats : [
+        {
+          id:'Bread Basket',
+          summary: "ssortment of fresh baked fruit breads and muffins $5.5"
+        },
+        {
+          id:'Granola',
+          summary: "Natural cereal of honey toasted oats, raisins, almonds and dates $8"
+        },
+        {
+          id:'Belgian Waffle',
+          summary: "Vanilla flavored batter with malted flour $10.5"
+        },
+        {
+          id:'Scrambled eggs',
+          summary: "Scrambled eggs, roasted red pepper and garlic, with green onions $7.5"
+        },
+        {
+          id:'Blueberry Pancakes',
+          summary: "With syrup, butter and lots of berries $8.5"
+        } 
+      ]
     };  
   }
   
