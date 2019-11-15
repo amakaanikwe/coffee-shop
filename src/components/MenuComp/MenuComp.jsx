3e6ed69626f6647f32da85041cb09814ab87b56b
Amaka Anikwe <amakaanikwe@gmail.com>
2019-11-14 23:31:43 -0500

    Added Menu comp

---
+++ src/components/MenuComp/MenuComp.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import CoffeeTeaComp from './CoffeeTeaComp.jsx';
+import TreatsComp from './TreatsComp.jsx';
+
+class MenuComp extends React.Component{
+      
+    render () {
+      return (
+        <div>
+          <table>
+            <tbody>
+                {this.props.coffee.map((item, i) => <CoffeeTeaComp key = {i} coffee = {item} />)}
+            </tbody>
+          </table>
+          <table>
+            <tbody>
+                {this.props.treats.map((item, i) => <TreatsComp key = {i} treats = {item} />)}
+            </tbody>
+          </table>
+          <button>
+            Order Now
+          </button>
+          {/* <Counters /> */}
+        </div>
+      )
+    }
+  
+  };
+  
+  export default MenuComp;
