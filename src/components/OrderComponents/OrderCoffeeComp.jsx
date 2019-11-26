99f2ca81133eccb7fcd50f2cfea69073ff8aedaf
Amaka Anikwe <amakaanikwe@gmail.com>
2019-11-25 19:11:01 -0500

    OrderCoffeeComp created

---
+++ src/components/OrderComponents/OrderCoffeeComp.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class OrderCoffeeComp extends React.Component {
+    render() {
+        return (
+            <div>
+               <tr>
+                   <td>{this.props.coffee.id}</td>
+               </tr>
+               <tr>
+                   <td>{this.props.coffee.summary}</td>
+                   <td>{this.props.coffee.price}</td>
+               </tr>
+               <br></br>
+           </div>
+        )
+    }
+}
+export default OrderCoffeeComp;
