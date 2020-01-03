21a4a29ce80336a23745e40f2449dae5610ded9a
Amaka Anikwe <amakaanikwe@gmail.com>
2020-01-02 22:03:17 -0500

    Style updates

--- src/components/OrderComponents/OrderCoffeeComp.jsx
+++ src/components/OrderComponents/OrderCoffeeComp.jsx
@@ -11,11 +11,13 @@ class OrderCoffeeComp extends React.Component {
                <tr className='row'>
                    <td className="col-sm-9">{this.props.coffeeItem.summary}</td>
                    <td className="col-sm-1 priceStyle">{this.props.coffeeItem.price}</td>
-                   <td className='col-sm-1 buttonStyle'>
+                   <td className='col-sm-1 btn-group-vertical buttonStyle'>
+                   <div className="btn-group-vertical">   
                        <button onClick={()=>this.props.onCoffeeIncrement(this.props.coffeeItem)} className='glyphicon glyphicon-menu-up'>
                         </button>
                         <button onClick={()=>this.props.onCoffeeDecrement(this.props.coffeeItem)} className='glyphicon glyphicon-menu-down'>
                         </button>
+                    </div>
                     </td>
                     <td className="col-sm-1 itemNumStyle">{this.props.coffeeItem.value}</td>
                     
--- src/components/OrderComponents/OrderTreatsComp.jsx
+++ src/components/OrderComponents/OrderTreatsComp.jsx
@@ -12,11 +12,13 @@ class OrderTreatsComp extends React.Component {
             <tr className='row'>
                 <td className="col-sm-9">{this.props.treatsItem.summary}</td>
                 <td className="col-sm-1 priceStyle">{this.props.treatsItem.price}</td>
-                <td className="col-sm-1 buttonStyle">                
+                <td className="col-sm-1 btn-group-vertical buttonStyle">  
+                    <div className="btn-group-vertical">      
                     <button onClick={()=>this.props.onTreatsIncrement(this.props.treatsItem)} className='glyphicon glyphicon-menu-up'>
                     </button>
                     <button onClick={()=>this.props.onTreatsDecrement(this.props.treatsItem)} className='glyphicon glyphicon-menu-down '>
                     </button>
+                    </div>  
                 </td>
                 <td className="col-sm-1 itemNumStyle">{this.props.treatsItem.value}</td>
                        
