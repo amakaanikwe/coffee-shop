53274f77bfce2f0c244e0faedea2721191c84122
Amaka Anikwe <amakaanikwe@gmail.com>
2020-06-20 23:22:15 -0400

    Updated summary comp

---
+++ src/components/SummaryComponents/ViewSumComp.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class ViewSumComp extends React.Component {
+    render() {
+      return (
+        <div>
+ 
+          <button>
+
+          </button>
+        </div>
+      );
+    }
+  }
+  
+  export default ViewSumComp;
