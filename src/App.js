5c84fc8d5b5beef6798eec0819fe377386df1832
Amaka Anikwe <amakaanikwe@gmail.com>
2020-07-06 08:33:54 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import MenuComp from "./components/MenuComponents/MenuComp.jsx";
 import OrderComp from "./components/OrderComponents/OrderComp.jsx";
 import coffeedata from "./inventory/coffeedata.json";
 import treatsdata from "./inventory/treatsdata.json";
-import { tsImportEqualsDeclaration } from "@babel/types";
 
 // let cooffeedata = require('./inventory/coffeedata.json');
 
@@ -20,26 +19,16 @@ class App extends React.Component {
     this.handleCompToggle = this.handleCompToggle.bind(this);
   }
 
-  // Order Coffee counters
+  // Order Increment Counters
   handleCoffeeIncrement = (coffeeItem) => {
-    const coffee = [...this.state.coffee];
+    const coffee = [...this.state.coffee ];
     const index = coffee.indexOf(coffeeItem);
     coffee[index] = { ...coffeeItem };
     coffee[index].value++;
+    console.log(coffee);
     this.setState({ coffee });
   };
 
-  handleCoffeeDecrement = (coffeeItem) => {
-    const coffee = [...this.state.coffee];
-    const index = coffee.indexOf(coffeeItem);
-    coffee[index] = { ...coffeeItem };
-    if (coffee[index].value >= 0) {
-      coffee[index].value--;
-    } else {coffee[index].value = 0 };
-    this.setState({ coffee });
-  };
-
-  // Order Treats Counters
   handleTreatsIncrement = (treatsItem) => {
     const treats = [...this.state.treats];
     const index = treats.indexOf(treatsItem);
@@ -48,16 +37,28 @@ class App extends React.Component {
     this.setState({ treats });
   };
 
-  handleTreatsDecrement = (treatsItem) => {
+  // Order Decrement Counters
+  handleCoffeeDecrement = (item) => {
+    const coffee = [...this.state.coffee];
+    const index = coffee.indexOf(item);
+    coffee[index] = { ...item };
+    if (coffee[index].value >= 0) {
+      coffee[index].value--;
+    } else {coffee[index].value = 0 };
+    this.setState({ coffee });
+  };
+
+  handleTreatsDecrement = (item) => {
     const treats = [...this.state.treats];
-    const index = treats.indexOf(treatsItem);
-    treats[index] = { ...treatsItem };
+    const index = treats.indexOf(item);
+    treats[index] = { ...item };
     treats[index].value--;
     if (treats[index].value >= 0) {
       treats[index].value--;
     } else {treats[index].value = 0 };
     this.setState({ treats });
   };
+
 
   // Component Toggle
   handleCompToggle() {
