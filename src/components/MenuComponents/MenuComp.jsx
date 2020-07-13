58e3b53ee6bf3241937aa669cb966a23f95c956d
Amaka Anikwe <amakaanikwe@gmail.com>
2020-07-12 23:44:11 -0400

    Updated menu comp

--- src/components/MenuComponents/MenuComp.jsx
+++ src/components/MenuComponents/MenuComp.jsx
@@ -2,43 +2,46 @@ import React from "react";
 import CoffeeTeaComp from "./CoffeeTeaComp.jsx";
 import TreatsComp from "./TreatsComp.jsx";
 import "../../css/MenuCompStyle.css";
+import "../../css/CoffeeTreatsCompStyle.css";
 
 class MenuComp extends React.Component {
   render() {
     return (
-      <div className="container-fluid menuStyle">
-        <div className="row">
-          <div className="col-sm-3">
-            <div className="sectionStyle">Coffee & Tea</div>
+      <React.Fragment>
+        <section className="container-fluid menuStyle">
+          <div className="row">
+            <div className="col-sm-3">
+              <div className="sectionStyle">Coffee & Tea</div>
+            </div>
           </div>
-        </div>
-        <table className="row">
-          <tbody className="col-sm-12">
-            <br></br>
-            {this.props.menu.coffee.map((item, i) => (
-              <CoffeeTeaComp key={i} coffee={item} />
-            ))}
-          </tbody>
-        </table>
-        <div className="row">
-          <div className="col-sm-3">
-            <br></br>
-            <br></br>
-            <div className="sectionStyle">Treats</div>
+          <table className="row">
+            <tbody className="col-sm-12">
+              <br></br>
+              {this.props.menu.coffee.map((item, i) => (
+                <CoffeeTeaComp key={i} coffee={item} />
+              ))}
+            </tbody>
+          </table>
+          <div className="row">
+            <div className="col-sm-3">
+              <br></br>
+              <br></br>
+              <div className="sectionStyle">Treats</div>
+            </div>
           </div>
-        </div>
-        <table className="row">
-          <tbody className="col-sm-12">
-            <br></br>
-            {this.props.menu.treats.map((item, i) => (
-              <TreatsComp key={i} treats={item} />
-            ))}
-          </tbody>
-        </table>
-        <button onClick={() => this.props.onCompToggle()}>
-          {!this.props.turnCompToggleOn ? "Order Now" : "Back to Menu"}
-        </button>
-      </div>
+          <table className="row">
+            <tbody className="col-sm-12">
+              <br></br>
+              {this.props.menu.treats.map((item, i) => (
+                <TreatsComp key={i} treats={item} className="compStyle"/>
+              ))}
+            </tbody>
+          </table>
+          <button onClick={() => this.props.onCompToggle()}>
+            {!this.props.turnCompToggleOn ? "Order Now" : "Back to Menu"}
+          </button>
+        </section>
+      </React.Fragment>
     );
   }
 }
