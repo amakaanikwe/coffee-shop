9042099c0617946418f4a7acce9fc6e5dd488a0e
Amaka Anikwe <amakaanikwe@gmail.com>
2020-07-19 23:45:22 -0400

    Updated reducers

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,9 +1,16 @@
-const reducer = functon(state, action){
-    if (action.type ==="") {
-        return state*action.payload;
-    }
-    if (action.type === "") {
+import React from "react";
+import {createStore} from "redux";
 
-    }
+const reducer = (state, action) => {
+    switch(action.type){
+        case "": {
+            state = {...state, name: action.payload}
+            break;
+        }
+        case "": {
+            state.age = action.payload;
+            break;
+        }
     return state;
+    }
 }
