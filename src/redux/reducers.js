bba816e2f3e1d05fb1a7d5f2d1303b8cbae2c6e8
Amaka Anikwe <amakaanikwe@gmail.com>
2020-07-21 06:03:55 -0400

    Updated reducers.js

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,9 @@
-import React from "react";
-import {createStore} from "redux";
+const initialState = {
+    counter: 0
+};
 
-const reducer = (state, action) => {
+
+const reducer = (state = initialState, action) => {
     switch(action.type){
         case "": {
             state = {...state, name: action.payload}
@@ -14,3 +16,5 @@ const reducer = (state, action) => {
     return state;
     }
 }
+
+export default reducer;
