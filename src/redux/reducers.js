3c6714dbef539cd6f8ecdda7c605902b51592012
Amaka Anikwe <amakaanikwe@gmail.com>
2020-07-21 18:45:17 -0400

    Update reducers.js

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,17 +1,28 @@
+import coffeedata from "../inventory/coffeedata.json";
+import treatsdata from "../inventory/treatsdata.json";
+
 const initialState = {
+    turnCompToggleOn: true,
+    coffee: coffeedata,
+    treats: treatsdata,
     counter: 0
 };
 
 
 const reducer = (state = initialState, action) => {
     switch(action.type){
-        case "": {
-            state = {...state, name: action.payload}
-            break;
+        case "COFFEEINCREMENT": {
+            state = {...state, name: action.payload};
         }
-        case "": {
+        case "TREATSINCREMENT": {
             state.age = action.payload;
-            break;
+
+        }
+        case "COFFEEDECREMENT": {
+
+        }
+        case "TREATSDECREMENT": {
+            
         }
     return state;
     }
