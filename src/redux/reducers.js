d003ae1c0f5a2b28351b36ac53ba2f9e3860b1f5
Amaka Anikwe <amakaanikwe@gmail.com>
2020-07-24 23:35:41 -0400

    Updated reducer setup

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,13 +1,3 @@
-import coffeedata from "../inventory/coffeedata.json";
-import treatsdata from "../inventory/treatsdata.json";
-
-const initialState = {
-    turnCompToggleOn: true,
-    coffee: coffeedata,
-    treats: treatsdata,
-    counter: 0
-};
-
 
 const reducer = (state = initialState, action) => {
     switch(action.type){
