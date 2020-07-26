481beee4bffb377bbcca7766129bc4035c5c33a2
Amaka Anikwe <amakaanikwe@gmail.com>
2020-07-26 15:25:13 -0400

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ import { Provider } from 'react-redux';
 import reducer from './redux/reducers'
 import './index.css';
 import App from './App';
+import coffeedata from "./inventory/coffeedata.json";
+import treatsdata from "./inventory/treatsdata.json";
+import { GET_AMOUNT } from './redux/actions';
 
 const initialStore = {
-    count: 0
+    coffeeMenu: coffeedata,
+    treatsMenu: treatsdata,
+    total: 0,
+    amount: 0
+
 }
 
 const store = createStore(reducer, initialStore);
