40bdcb42d0827deae81550f849d510453bb3a056
Amaka Anikwe <amakaanikwe@gmail.com>
2020-07-27 03:06:52 -0400

    Added NavBar comp

---
+++ src/components/MenuComponents/NavBar.jsx
@@ -0,0 +1,15 @@
+
+import React, { useState } from 'react';
+
+function NavBar() {
+
+
+  return (
+    <nav>
+        <h1 className="text-center">Our Menu</h1>
+        <p></p>
+    </nav>
+  );
+}
+
+export default NavBar;
