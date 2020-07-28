e74dfd824ea9910b660d565f1c10a866c3ce7a3c
Amaka Anikwe <amakaanikwe@gmail.com>
2020-07-28 18:19:16 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,90 +3,70 @@ import MenuComp from "./components/MenuComponents/MenuComp.jsx";
 import OrderComp from "./components/OrderComponents/OrderComp.jsx";
 import coffeedata from "./inventory/coffeedata.json";
 import treatsdata from "./inventory/treatsdata.json";
+import { useSelector} from "react-redux";
 
-// let cooffeedata = require('./inventory/coffeedata.json');
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () =>{
+    const turnCompToggleOn = useSelector (state => state.turnCompToggleOn);
 
-    this.state = {
-      turnCompToggleOn: true,
-      coffee: coffeedata,
-      treats: treatsdata,
-    };
+    // const state = {
+    //   turnCompToggleOn: true,
+    //   coffee: coffeedata,
+    //   treats: treatsdata,
+    // };
 
-    this.handleCompToggle = this.handleCompToggle.bind(this);
-  }
 
   // Order Increment Counters
-  handleCoffeeIncrement = (coffeeItem) => {
-    const coffee = [...this.state.coffee ];
-    const index = coffee.indexOf(coffeeItem);
-    coffee[index] = { ...coffeeItem };
-    coffee[index].value++;
-    console.log(coffee);
-    this.setState({ coffee });
-  };
+  // handleCoffeeIncrement = (coffeeItem) => {
+  //   const coffee = [...this.state.coffee ];
+  //   const index = coffee.indexOf(coffeeItem);
+  //   coffee[index] = { ...coffeeItem };
+  //   coffee[index].value++;
+  //   console.log(coffee);
+  //   this.setState({ coffee });
+  // };
 
-  handleTreatsIncrement = (treatsItem) => {
-    const treats = [...this.state.treats];
-    const index = treats.indexOf(treatsItem);
-    treats[index] = { ...treatsItem };
-    treats[index].value++;
-    this.setState({ treats });
-  };
+  // handleTreatsIncrement = (treatsItem) => {
+  //   const treats = [...this.state.treats];
+  //   const index = treats.indexOf(treatsItem);
+  //   treats[index] = { ...treatsItem };
+  //   treats[index].value++;
+  //   this.setState({ treats });
+  // };
 
   // Order Decrement Counters
-  handleCoffeeDecrement = (item) => {
-    const coffee = [...this.state.coffee];
-    const index = coffee.indexOf(item);
-    coffee[index] = { ...item };
-    if (coffee[index].value >= 0) {
-      coffee[index].value--;
-    } else {coffee[index].value = 0 };
-    this.setState({ coffee });
-  };
+  // handleCoffeeDecrement = (item) => {
+  //   const coffee = [...this.state.coffee];
+  //   const index = coffee.indexOf(item);
+  //   coffee[index] = { ...item };
+  //   if (coffee[index].value >= 0) {
+  //     coffee[index].value--;
+  //   } else {coffee[index].value = 0 };
+  //   this.setState({ coffee });
+  // };
 
-  handleTreatsDecrement = (item) => {
-    const treats = [...this.state.treats];
-    const index = treats.indexOf(item);
-    treats[index] = { ...item };
-    treats[index].value--;
-    if (treats[index].value >= 0) {
-      treats[index].value--;
-    } else {treats[index].value = 0 };
-    this.setState({ treats });
-  };
+  // handleTreatsDecrement = (item) => {
+  //   const treats = [...this.state.treats];
+  //   const index = treats.indexOf(item);
+  //   treats[index] = { ...item };
+  //   treats[index].value--;
+  //   if (treats[index].value >= 0) {
+  //     treats[index].value--;
+  //   } else {treats[index].value = 0 };
+  //   this.setState({ treats });
+  // };
 
 
-  // Component Toggle
-  handleCompToggle() {
-    this.setState((prevState) => ({
-      turnCompToggleOn: !prevState.turnCompToggleOn,
-    }));
-  }
-
-  render() {
     return (
-      <div>
         <div>
-          {this.state.turnCompToggleOn ? (
-            <MenuComp menu={this.state} onCompToggle={this.handleCompToggle} />
+          {turnCompToggleOn ? (
+            <MenuComp />
           ) : (
-            <OrderComp
-              menu={this.state}
-              onCoffeeIncrement={this.handleCoffeeIncrement}
-              onCoffeeDecrement={this.handleCoffeeDecrement}
-              onTreatsIncrement={this.handleTreatsIncrement}
-              onTreatsDecrement={this.handleTreatsDecrement}
-              onCompToggle={this.handleCompToggle}
-            />
+            <OrderComp />
           )}
         </div>
-      </div>
     );
-  }
+
 }
 
 export default App;
