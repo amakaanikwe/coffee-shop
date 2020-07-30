eed45dfd9e3d649d8ea08d4a4f1a92652ecb2536
Amaka Anikwe <amakaanikwe@gmail.com>
2020-07-29 22:23:48 -0400

    Refactored TreatsComp

--- src/components/MenuComponents/TreatsComp.jsx
+++ src/components/MenuComponents/TreatsComp.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import "../../css/CoffeeTreatsCompStyle.css";
+import { useSelector } from "react-redux";
 
-class TreatsComp extends React.Component {
-  render() {
+const TreatsComp = (treats) => {
+
     return (
       <>
         <tr className="row compStyle">
-          <td className="col-sm-12">{this.props.treats.id}</td>
+          <td className="col-sm-12">{treats.treats.id}</td>
         </tr>
         <tr className="row">
-          <td className="col-sm-10">{this.props.treats.summary}</td>
-          <td className="col-sm-2">{this.props.treats.price}</td>
+          <td className="col-sm-10">{treats.treats.summary}</td>
+          <td className="col-sm-2">{treats.treats.price}</td>
         </tr>
         <br></br>
       </>
     );
   }
-}
+
 
 export default TreatsComp;
