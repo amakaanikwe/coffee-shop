ccad0181b77ae0721a3dd2b73d8e3c11c9a76a44
Amaka Anikwe <amakaanikwe@gmail.com>
2020-07-30 23:02:59 -0400

    Updated reducers.js

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,4 @@
-import  { COFFEE_INCREASE, COFFEE_DECREASE, TREATS_INCREASE, TREATS_DECREASE, REMOVE} from "./actions";
+import  { COFFEE_INCREASE, COFFEE_DECREASE, TREATS_INCREASE, TREATS_DECREASE, REMOVE, HANDLE_COMP_TOGGLE} from "./actions";
 
 const reducer = (state, action) => {
     switch(action.type) {
@@ -12,13 +12,25 @@ const reducer = (state, action) => {
         return {...state, count: state.treate.value + 1 }
         
         case TREATS_DECREASE : 
-            return {...state}
+            return {...state, count: state.treate.value - 1   }
         
         case REMOVE :
             return {...state, cart: []}
+        case HANDLE_COMP_TOGGLE :
+            return {...state, turnCompToggleOn: !state.turnCompToggleOn}
+            // Component Toggle
+            // const handleCompToggle = () => {
+            //     this.setState((prevState) => ({
+            //       turnCompToggleOn: !prevState.turnCompToggleOn,
+            //     }));
+            //   }
+
         default:
             return state;
+
     }
 }
 
 export default reducer;
+
+
