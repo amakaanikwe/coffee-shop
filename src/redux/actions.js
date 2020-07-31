58182bc2cea400d22dc15d710df5629169cb0014
Amaka Anikwe <amakaanikwe@gmail.com>
2020-07-30 23:02:25 -0400

    Updated actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,6 +2,7 @@ export const COFFEE_INCREASE = "COFFEE_INCREASE";
 export const COFFEE_DECREASE = "COFFEE_DECREASE";
 export const TREATS_INCREASE = "TREATS_INCREASE";
 export const TREATS_DECREASE = "TREATS_DECREASE";
+export const HANDLE_COMP_TOGGLE = "HANDLE_COMP_TOGGLE";
 export const REMOVE = "REMOVE";
 export const CLEAR_CART  = "CLEAR_CART";
 export const GET_TOTAL = "GET_TOTAL";
