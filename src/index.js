7a3751adb28308fdada161b901f14910c73c75b4
Amaka Anikwe <amakaanikwe@gmail.com>
2020-08-09 19:47:35 -0400

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import coffeedata from "./inventory/coffeedata.json";
-import treatsdata from "./inventory/treatsdata.json";
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './redux/reducers'
 import './index.css';
 import App from './App';
 
-const initialState = {
-    coffeeMenu: coffeedata,
-    treatsMenu: treatsdata,
-    turnCompToggleOn: true,
-    total: 0,
-    amount: 0
-
-}
 
 
-const store = createStore(reducer, initialState);
+const store = createStore(reducer);
 
 
 
