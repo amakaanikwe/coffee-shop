b34dee63b510ab0c30b13492e198b27f94cf3e5e
Amaka Anikwe <amakaanikwe@gmail.com>
2020-08-10 16:46:46 -0400

    Updated OrderTreatsComp.jsx

--- src/components/OrderComponents/OrderTreatsComp.jsx
+++ src/components/OrderComponents/OrderTreatsComp.jsx
@@ -1,45 +1,62 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import {TREATS_INCREASE, TREATS_DECREASE} from "../../redux/actionsTypes.js";
 import "../../css/OrderCompStyle.css";
 
-const OrderTreatsComp = (treats) => {
+const OrderTreatsComp = ({id, summary, price, value}) => {
   const treatsMenu = useSelector(state => state.treatsMenu);
+  const dispatch = useDispatch();
 
 
-  const formatCount = () => {
-    // return this.state.count === 0 ? 'zero' : this.state.count;
-    // jsx expressions can be used the same way as objects
-    const value  = treatsMenu[0].value;
+  // const formatCount = () => {
+  //   // return this.state.count === 0 ? 'zero' : this.state.count;
+  //   // jsx expressions can be used the same way as objects
+  //   const value  = treatsMenu[0].value;
 
-    return (value === 0) | (value <= 0) ? 0 : value;
+  //   return (value === 0) | (value <= 0) ? 0 : value;
+  // }
+
+  function incrimentTreatsCout (treatsId){
+    dispatch({
+      type: TREATS_INCREASE,
+      payload: treatsId
+    })
+
+  }
+
+  function decrementTreatsCout (treatsId){
+    dispatch({
+      type: TREATS_DECREASE,
+      payload: treatsId
+    })   
   }
 
     return (
       <>
         <tr className="row style">
-          <td className="col-sm-12">{treats.treats.id}</td>
+          <td className="col-sm-12">{id}</td>
         </tr>
         <tr className="row">
-          <td className="col-sm-9">{treats.treats.summary}</td>
-          <td className="col-sm-1 priceStyle">{treats.treats.price}</td>
+          <td className="col-sm-9">{summary}</td>
+          <td className="col-sm-1 priceStyle">{price}</td>
           <td className="col-sm-1 btn-group-vertical buttonStyle">
             <div className="btn-group-vertical">
               <button
                 onClick={() =>
-                  {}
+                  {incrimentTreatsCout(id)}
                 }
                 className="glyphicon glyphicon-menu-up"
               ></button>
               <button
                 onClick={() =>
-                 {}
+                 {decrementTreatsCout(id)}
                 }
                 className="glyphicon glyphicon-menu-down "
               ></button>
             </div>
           </td>
           <td className="col-sm-1 itemNumStyle">
-            <span>{formatCount()}</span>
+            <span>{value}</span>
           </td>
         </tr>
         <br></br>
